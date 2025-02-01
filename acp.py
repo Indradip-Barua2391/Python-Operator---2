09b4758027ae5a858c7d55c1e0e6efcaa92ebe75
Indradip <chadsigmarizzler@gmail.com>
2025-02-01 20:12:34 +0600

    abc

---
+++ acp.py
@@ -0,0 +1,10 @@
+letter = input("Enter A Letter: ")
+
+if letter >= "a" and letter <= "z":
+    print(f"the letter {letter} is a Lower Case Alphabet")
+    
+elif letter >= "A" and letter <= "Z":
+    print(f"the letter {letter} is a Uppercase letter Alphabet")
+    
+else:
+    print(f"the letter {letter} is NOTTT an alphabet")
